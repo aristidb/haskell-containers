c3e6c4bd3981239733326bf3042d01c6a9aef676
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-05-16 18:55:31 +0200

    Initial import

---
+++ AssociativeContainer.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FunctionalDependencies #-}
+
+module AssociativeContainer
+(
+ AssociativeContainer
+)
+where
+
+import Control.Monad
+import qualified Data.Map as M
+
+class Eq k => AssociativeContainer a k v | a -> k, a -> v where
+    toAList :: a -> [(k, v)]
+    fromAList :: [(k, v)] -> a
+    insert :: (k, v) -> a -> a
+    empty :: a
+    lookup :: (MonadPlus t) => k -> a -> t v
+    merge :: a -> a -> a
+    mergeAList :: [(k, v)] -> a -> a
+    mergeAList = merge . fromAList
+
+instance Eq a => AssociativeContainer [(a, b)] a b where
+    fromAList = id
+    toAList = id
+    empty = []
+    insert = (:)
+    lookup k = msum . map check
+        where check (k', v) = guard (k == k') >> return v
+    merge = (++)
+
+instance (Eq a, Ord a) => AssociativeContainer (M.Map a b) a b where
+    fromAList = M.fromList
+    toAList = M.toList
+    insert = uncurry M.insert
+    empty = M.empty
+    lookup k m = unmaybe $ M.lookup k m
+        where unmaybe Nothing = mzero
+              unmaybe (Just a) = return a
+    merge = M.union
