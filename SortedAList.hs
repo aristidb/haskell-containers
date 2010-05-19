a225c3a5127d18ae21b7937f3fd7586e40c05f4c
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2010-05-20 01:50:34 +0200

    formatting

--- SortedAList.hs
+++ SortedAList.hs
@@ -18,13 +18,15 @@ fromSortedAList :: SortedAList k v -> [(k, v)]
 fromSortedAList (SortedAList xs) = xs
 
 removeDuplicates (SortedAList xs) = SortedAList $ foldr removeDuplicate [] xs
-    where removeDuplicate x@(k, _) ys@(y@(k', _) : _) | k == k' = ys
-                                                      | otherwise = x : ys
+    where removeDuplicate x@(k, _) ys@(y@(k', _) : _)
+              | k == k' = ys
+              | otherwise = x : ys
           removeDuplicate x _ = [x]
 
 removeDuplicates' (SortedAList xs) = SortedAList $ foldr removeDuplicate [] xs
-    where removeDuplicate x@(k, _) ys@(y@(k', _) : ys') | k == k' = x : ys'
-                                                        | otherwise = x : ys
+    where removeDuplicate x@(k, _) ys@(y@(k', _) : ys')
+              | k == k' = x : ys'
+              | otherwise = x : ys
           removeDuplicate x _ = [x]
 
 instance Ord a => AssociativeContainer (SortedAList a b) a b where
@@ -32,20 +34,19 @@ instance Ord a => AssociativeContainer (SortedAList a b) a b where
     fromAList = SortedAList . L.sortBy (compare `on` fst)
     empty = SortedAList []
     insert x (SortedAList m) = SortedAList (insert' x m)
-        where insert' x@(k, v) m@(p@(k', v') : xs) | k < k' = x : m
-                                                   | otherwise = insert' x m
+        where insert' x@(k, v) m@(p@(k', v') : xs)
+                  | k < k' = x : m
+                  | otherwise = insert' x m
     null = Prelude.null . fromSortedAList
     lookup k = unlist . range . fromSortedAList
-        where 
-          unlist = msum . map (return . snd)
-          range = takeWhile (keyEquals k) . dropWhile (keyNotEquals k)
+        where unlist = msum . map (return . snd)
+              range = takeWhile (keyEquals k) . dropWhile (keyNotEquals k)
     remove k (SortedAList m) = SortedAList (lower ++ upper)
-        where
-          (lower, upper') = break (keyEquals k) m
-          upper = dropWhile (keyEquals k) upper'
+        where (lower, upper') = break (keyEquals k) m
+              upper = dropWhile (keyEquals k) upper'
     merge (SortedAList x) (SortedAList y) = SortedAList (merge' x y)
-        where
-          merge' xs@(x@(k1, _) : xs') ys@(y@(k2, _) : ys') | k1 <= k2 = x : merge' xs' ys
-                                                           | otherwise = y : merge' xs ys'
-          merge' _ ys = ys
+        where merge' xs@(x@(k1, _) : xs') ys@(y@(k2, _) : ys')
+                  | k1 <= k2 = x : merge' xs' ys
+                  | otherwise = y : merge' xs ys'
+              merge' _ ys = ys
 
